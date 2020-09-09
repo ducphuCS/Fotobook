18ea2f48b369fa54d593fa3f3cda8fcfa5470227
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-09-09 14:34:38 +0700

    update photo uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -10,6 +10,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 200]
   end
 
+  version :avatar do
+    process :resize_to_fill => [32, 32]
+  end
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
