1d6deba3e285c973fb85363af396add61da53679
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-09-10 17:07:51 +0700

    config test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,4 +46,15 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'phu.nguyen.cse@hcmut.edu.vn',
+    password: 'CSEbk1906',
+    authentication: 'plain'
+    # enable_starttls_auto: true
+  }
 end
