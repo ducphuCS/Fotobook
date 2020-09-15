62aaa1497e0b443568925f6297e0d7caabcd9e49
ducphuCS <phu.nguyen.cse@hcmut.edu.vn>
2020-09-15 11:40:54 +0700

    update cloudinary

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,12 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :file
+  include Cloudinary::CarrierWave
+
+  if Rails.env.production
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
